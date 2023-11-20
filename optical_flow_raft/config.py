abbdbf04d12db1d5028c509e162741644fa9fd2e
Danielle Ben Bashat <daniellebenba@gmail.com>
2023-11-20 16:19:15 +0200

    update the gcs cnf

--- optical_flow_raft/config.py
+++ optical_flow_raft/config.py
@@ -1,7 +1,7 @@
 from optical_flow_raft.utils.flow_utils import make_color_wheel
 
-BUCKET_NAME = 'datasets-reteai'
-PROJECT_ID = 'splendid-flow-231921'
+BUCKET_NAME = 'tl-private-datasets'
+PROJECT_ID = 'tl-private-dev-project'
 COLORWHEEL = make_color_wheel()
 IMG_SIZE = (440, 1024)
 MAX_SCENE = 300
