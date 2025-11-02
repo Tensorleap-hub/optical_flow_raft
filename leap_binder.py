fc2073aae955c1d18cf696ca9a0d93e44f521067
yamtawachi <yam.tawachi@tensorleap.ai>
2025-11-02 14:46:14 +0200

    Code mapping (#4)
    * decorators and code mapping
    
    * clean version
    
    * Update code-loader version to ^1.0.145
    
    * Remove code-loader dependency from requirements
    
    ---------
    
    Co-authored-by: Tom Koren <tom.koren@tensorleap.ai>

--- leap_binder.py
+++ leap_binder.py
@@ -11,6 +11,9 @@ from code_loader.contract.visualizer_classes import LeapImage
 from code_loader.contract.enums import (
     LeapDataType
 )
+from code_loader.inner_leap_binder.leapbinder_decorators import tensorleap_preprocess, tensorleap_input_encoder, \
+    tensorleap_gt_encoder, tensorleap_metadata, tensorleap_custom_visualizer, tensorleap_custom_loss, \
+    tensorleap_custom_metric
 from numpy import ndarray
 
 from optical_flow_raft.config import BUCKET_NAME, MAX_SCENE, MAX_STEREO, IMG_SIZE
@@ -20,7 +23,7 @@ from optical_flow_raft.utils.gcs_utils import download
 
 
 # --------------------------------------------------inputs & GT---------------------------------------------------------
-
+@tensorleap_preprocess()
 def subset_images() -> List[PreprocessResponse]:
     scene_flow = get_kitti_data(bucket_name=BUCKET_NAME, data_subset="scene")
     stereo_flow = get_kitti_data(bucket_name=BUCKET_NAME, data_subset="stereo")
@@ -48,20 +51,21 @@ def get_image(cloud_path: str) -> np.ndarray:
     return flow_img
 
 
+@tensorleap_input_encoder('image1', channel_dim=1)
 def input_image1(idx: int, data: PreprocessResponse) -> np.ndarray:
     data = data.data
     path0 = data['paths'][idx][0]
     img0 = get_image(path0)
-    return img0
+    return np.transpose(img0, [2,0,1]).astype(np.float32)
 
-
+@tensorleap_input_encoder('image2', channel_dim=1)
 def input_image2(idx: int, data: PreprocessResponse) -> np.ndarray:
     data = data.data
     path1 = data['paths'][idx][1]
     img1 = get_image(path1)
-    return img1
+    return np.transpose(img1, [2,0,1]).astype(np.float32)
 
-
+@tensorleap_gt_encoder('mask')
 def gt_encoder(idx: int, data: PreprocessResponse) -> np.ndarray:
     data = data.data
     path = data['paths'][idx][2]
@@ -91,7 +95,7 @@ def mu_over_sigma_of(gt: np.ndarray) -> float:
     else:
         return mean
 
-
+@tensorleap_metadata('metadata_dict')
 def metadata_dict(idx: int, data: PreprocessResponse) -> Dict[str, Union[float, int, str]]:
     gt = gt_encoder(idx, data)
     res = {
@@ -109,7 +113,7 @@ def poe_x(filename: str, data: PreprocessResponse) -> float:
 def poe_y(filename: str, data: PreprocessResponse) -> float:
     return data.data['poe'][filename][1]
 
-
+@tensorleap_metadata('metadata_focus_of_expansion')
 def metadata_focus_of_expansion(idx: int, data: PreprocessResponse) -> Dict[str, Union[float, int, str]]:
     filename = metadata_filename(idx, data)
     res = {
@@ -118,119 +122,101 @@ def metadata_focus_of_expansion(idx: int, data: PreprocessResponse) -> Dict[str,
     }
     return res
 
-
+@tensorleap_metadata('filename')
 def metadata_filename(idx: int, data: PreprocessResponse) -> str:
     data = data.data
     path = data['paths'][idx][0]
     fname = path.split('/')[-1]
     return fname
 
-
+@tensorleap_metadata('dataset name')
 def dataset_name(idx: int, subset: PreprocessResponse) -> str:
     return subset.data['dataset_name']
 
-
+@tensorleap_metadata('idx')
 def metadata_idx(idx: int, data: PreprocessResponse) -> int:
     return idx
 
 
 # -------------------------------------------------------- visualizers -------------------------------------------------
 
-
+@tensorleap_custom_visualizer('image_visualizer', LeapDataType.Image)
 def image_visualizer(image: npt.NDArray[np.float32]) -> LeapImage:
-    return LeapImage(image[..., ::-1])
+    image = np.squeeze(image)
+    return LeapImage(np.transpose(image, [1,2,0])[..., ::-1].astype(np.uint8))
 
-
+@tensorleap_custom_visualizer('flow_visualizer', LeapDataType.Image)
 def flow_visualizer(flow: npt.NDArray[np.float32]) -> LeapImage:
+    flow = np.squeeze(flow)
+    if flow.shape[0] == 2 or flow.shape[0] ==3: #channel first to last
+        flow = np.transpose(flow, [1,2,0])
     img = flow_to_image(flow)
     return LeapImage(img)
 
-
+@tensorleap_custom_visualizer('gt_visualizer', LeapDataType.Image)
 def gt_visualizer(flow: npt.NDArray[np.float32]) -> LeapImage:
     img = flow_to_image(flow)
     img[(img == 255).all(axis=-1)] = 0
     return LeapImage(img)
 
-
+@tensorleap_custom_visualizer('fg_visualizer', LeapDataType.Image)
 def mask_visualizer(mask: npt.NDArray[np.uint8]) -> LeapImage:
+    mask = np.squeeze(mask)
     return LeapImage((mask[..., None].repeat(3, axis=2) * 255).astype(np.uint8))
 
 
 # -------------------------------------------------------- metrics  -------------------------------------------------
 
-
-def EPE(gt_flow: tf.Tensor, pred_flow: tf.Tensor) -> tf.Tensor:
-    # gt_flow shape: (batch_size, height, width, 2)
-    # pred_flow shape: (batch_size, height, width, 2)
-    # v_gt - v_pred u_gt - u_pred
+@tensorleap_custom_loss('EPE')
+def EPE(gt_flow: np.ndarray, pred_flow: np.ndarray) -> tf.Tensor:
+    gt_flow = tf.convert_to_tensor(gt_flow)
+    pred_flow = np.transpose(pred_flow, [0, 2, 3, 1])
+    pred_flow = tf.convert_to_tensor(pred_flow)
     pixel_err = EPE_mask(gt_flow, pred_flow)
     sample_err = tf.reduce_mean(pixel_err, axis=[1, 2])
-    return sample_err
+    return sample_err.numpy()
 
-
+@tensorleap_gt_encoder('fg_mask')
 def fg_mask(idx: int, subset: PreprocessResponse) -> np.ndarray:
     if subset.data['dataset_name'] == 'scene_flow':
         filename = metadata_filename(idx, subset)
         local_file = download(f"KITTI/data_scene_flow/training/obj_map/{filename}", bucket_name=BUCKET_NAME)
         return np.array(Image.open(local_file).resize(IMG_SIZE[::-1], Image.NEAREST)).astype(np.float32)
     elif subset.data['dataset_name'] == 'stereo_flow':
-        return np.ones_like(input_image1(idx, subset)[..., 0]).astype(np.float32)
+        return np.ones_like(np.transpose(input_image1(idx, subset), [1,2,0])[..., 0]).astype(np.float32)
 
-
-def fl_metric(gt_flow: tf.Tensor, pred_flow: tf.Tensor) -> tf.Tensor:
+@tensorleap_custom_metric('FL-all')
+def fl_metric(gt_flow: np.ndarray, pred_flow: np.ndarray) -> np.ndarray:
+    gt_flow = tf.convert_to_tensor(gt_flow)
+    pred_flow = np.transpose(pred_flow, [0, 2, 3, 1])# Channel First to Last
+    pred_flow = tf.convert_to_tensor(pred_flow)
     fl_map = get_fl_map(gt_flow, pred_flow)
     outliers_num = tf.math.count_nonzero(fl_map, axis=[1, 2])
-    return outliers_num / (tf.maximum(tf.math.count_nonzero(gt_flow[..., -1], axis=[1, 2]), 1))
+    return (outliers_num / (tf.maximum(tf.math.count_nonzero(gt_flow[..., -1], axis=[1, 2]), 1))).numpy()
 
-
-def fl_foreground(gt_flow: tf.Tensor, pred_flow: tf.Tensor, foreground_map: tf.Tensor) -> tf.Tensor:
+@tensorleap_custom_metric('FL-fg')
+def fl_foreground(gt_flow: np.ndarray, pred_flow: np.ndarray, foreground_map: np.ndarray) -> np.ndarray:
+    gt_flow = tf.convert_to_tensor(gt_flow)
+    pred_flow = np.transpose(pred_flow, [0, 2, 3, 1])# Channel First to Last
+    pred_flow = tf.convert_to_tensor(pred_flow)
+    foreground_map = tf.convert_to_tensor(foreground_map)
     fl_map = tf.cast(get_fl_map(gt_flow, pred_flow), float) * foreground_map
     outliers_num = tf.math.count_nonzero(fl_map, axis=[1, 2])
     combined_mask = gt_flow[..., -1] * foreground_map
-    return outliers_num / (tf.maximum(tf.math.count_nonzero(combined_mask, axis=[1, 2]), 1))
+    return (outliers_num / (tf.maximum(tf.math.count_nonzero(combined_mask, axis=[1, 2]), 1))).numpy()
 
-
-def fl_background(gt_flow: tf.Tensor, pred_flow: tf.Tensor, foreground_map: tf.Tensor) -> tf.Tensor:
+@tensorleap_custom_metric('FL-bg')
+def fl_background(gt_flow: np.ndarray, pred_flow: np.ndarray, foreground_map: np.ndarray) -> np.ndarray:
+    gt_flow = tf.convert_to_tensor(gt_flow)
+    pred_flow = np.transpose(pred_flow, [0, 2, 3, 1]) # Channel First to Last
+    pred_flow = tf.convert_to_tensor(pred_flow)
+    foreground_map = tf.convert_to_tensor(foreground_map)
     background_mask = 1 - foreground_map
     fl_map = tf.cast(get_fl_map(gt_flow, pred_flow), float) * background_mask
     outliers_num = tf.math.count_nonzero(fl_map, axis=[1, 2])
     combined_mask = gt_flow[..., -1] * background_mask
-    return outliers_num / (tf.maximum(tf.math.count_nonzero(combined_mask, axis=[1, 2]), 1))
+    return (outliers_num / (tf.maximum(tf.math.count_nonzero(combined_mask, axis=[1, 2]), 1))).numpy()
 
-
-# -------------------------------------------------------- binding  -------------------------------------------------
-# preprocess function
-leap_binder.set_preprocess(subset_images)
-
-# set input and gt
-leap_binder.set_input(input_image1, 'image1')
-leap_binder.set_input(input_image2, 'image2')
-leap_binder.set_input(fg_mask, 'fg_mask')
-leap_binder.set_ground_truth(gt_encoder, 'mask')
-
-# set prediction
-leap_binder.add_prediction('opt_flow', ['x', 'y'])
-
-# set meata_data
-leap_binder.set_metadata(metadata_idx, 'idx')
-leap_binder.set_metadata(metadata_filename, 'filename')
-leap_binder.set_metadata(dataset_name, 'dataset name')
-leap_binder.set_metadata(metadata_focus_of_expansion, 'metadata_focus_of_expansion')
-leap_binder.set_metadata(metadata_dict, 'metadata_dict')
-
-# set visualizer
-leap_binder.set_visualizer(image_visualizer, 'image_visualizer', LeapDataType.Image)
-leap_binder.set_visualizer(flow_visualizer, 'flow_visualizer', LeapDataType.Image)
-leap_binder.set_visualizer(gt_visualizer, 'gt_visualizer', LeapDataType.Image)
-leap_binder.set_visualizer(mask_visualizer, 'fg_visualizer', LeapDataType.Image)
-
-# set loss
-leap_binder.add_custom_loss(EPE, 'EPE')
-
-# set custom metrics
-leap_binder.add_custom_metric(fl_metric, 'FL-all')
-leap_binder.add_custom_metric(fl_foreground, 'FL-fg')
-leap_binder.add_custom_metric(fl_background, 'FL-bg')
 
 if __name__ == '__main__':
     leap_binder.check()
---
+++ leap_integration.py
@@ -0,0 +1,63 @@
+import os
+from leap_binder import *
+from os import environ
+from code_loader.inner_leap_binder.leapbinder_decorators import tensorleap_load_model, tensorleap_integration_test
+from code_loader.contract.datasetclasses import PredictionTypeHandler
+from code_loader.plot_functions.visualize import visualize
+import onnxruntime
+
+prediction_type1 = PredictionTypeHandler('opt_flow', ["x", "y"])
+
+@tensorleap_load_model([prediction_type1])
+def load_model():
+    dir_path = os.path.dirname(os.path.abspath(__file__))
+    model_path = 'model/raft_new.onnx'
+    sess = onnxruntime.InferenceSession(os.path.join(dir_path, model_path))
+    return sess
+
+
+@tensorleap_integration_test()
+def check_custom_integration(idx, subset):  # This test requires the relevant secret to be loaded to the system environment AUTH_SECRET
+    if environ.get('AUTH_SECRET') is None:
+        print("The AUTH_SECRET system variable must be initialized with the relevant secret to run this test")
+        exit(-1)
+    print("started custom tests")
+    sess = load_model()
+    # get inputs
+    img_1 = input_image1(idx, subset)
+    img_2 = input_image2(idx, subset)
+    input_name_1 = sess.get_inputs()[0].name
+    input_name_2 = sess.get_inputs()[1].name
+    pred = sess.run(None, {input_name_1: img_1,
+                                   input_name_2: img_2})[0]
+    gt = gt_encoder(idx, subset)
+    foreground_mask = fg_mask(idx, subset)
+
+    # get all metadata
+    idx = metadata_idx(idx, subset)
+    filename = metadata_filename(idx, subset)
+    name = dataset_name(idx, subset)
+    focus_of_expansion = metadata_focus_of_expansion(idx, subset)
+    metadata_all = metadata_dict(idx, subset)
+
+    # run all visualizers and plot them. Visualizers run on results without the Batch dimension.
+    img_vis = image_visualizer(img_1)
+    visualize(img_vis)
+    pred_flow_vis = flow_visualizer(pred)
+    visualize(pred_flow_vis)
+    gt_flow_vis = flow_visualizer(gt)
+    visualize(gt_flow_vis)
+    mask_vis = mask_visualizer(foreground_mask)
+    visualize(mask_vis)
+    # run losses and EPE
+    loss = EPE(gt, pred)
+    sample_fl_metric = fl_metric(gt, pred)
+    sample_fl_fg_metric = fl_foreground(gt, pred, foreground_mask)
+    sample_fl_bg_metric = fl_background(gt, pred, foreground_mask)
+    print("Custom tests finished successfully")
+
+
+if __name__ == "__main__":
+    subsets = subset_images()
+    check_custom_integration(0, subsets[0])
+leap_binder.add_prediction('opt_flow', ['x', 'y'])
